113841fff648ab3eaf64e1fec829476add2acfff
Rachit Patel <rachitp509@gmail.com>
2024-02-18 16:50:30 +0530

    bug fixes of socket

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ const server = app.listen(port, () => {
 
 const io = require("socket.io")(server, {
   cors: {
+    // origin: "http://localhost:3000",
     origin: "https://chat-front-chi.vercel.app",
     credentials: true,
   },
